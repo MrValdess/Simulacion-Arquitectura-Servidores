577465710de69ba7382a16eea5126a9d03d7a1d0
Pablo Garcia <pablocns2002@gmail.com>
2026-04-28 10:09:10 +0200

    Quitar php
    Quitamos php a nuestro servidor para mayor facilidad para levantar nuestra arquitectura

--- Production/drupalinit.sh
+++ Production/drupalinit.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ ! -d "/app/web" ]; then
+echo "Iniciando contenedor Drupal..."
+
+apt update && apt install -y unzip default-mysql-client
+
+# Crear proyecto Drupal si no existe
+if [ ! -d "/var/www/html/web" ]; then
     echo "Proyecto Drupal no encontrado, creando..."
-    apt update
-    apt install unzip
     rm 50x.html && rm index.html
     composer create-project drupal/recommended-project .
     chown -R daemon:daemon /app/
@@ -11,8 +14,9 @@ if [ ! -d "/app/web" ]; then
     echo "Proyecto Drupal creado"
 else
     echo "Proyecto Drupal encontrado, no se requiere instalacion. Continuando..."
-    apt update
 fi
 
 useradd -m -s /bin/bash johndoe
-exec php-fpm -F
+
+echo "Iniciando Apache..."
+exec apache2-foreground
