cf2ced5141b3e0914d21ed005dd82d133068e4ae
Pablo Garcia <pablocns2002@gmail.com>
2026-05-04 12:25:24 +0200

    VPN cambios
    Genera automáticamente los archivos de usuario para más facil comprobacion. Actualizacion de gitignore

--- vpn/start.sh
+++ vpn/start.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
-
 set -e
 
 OVPN_DIR="/etc/openvpn"
 
 echo "[+] Iniciando contenedor OpenVPN..."
 
-# Si no existe configuración → inicializar
+# Inicialización 
 if [ ! -f "$OVPN_DIR/openvpn.conf" ]; then
     echo "[+] Primera ejecución: generando configuración..."
 
     ovpn_genconfig -u udp://localhost
-    sed -i 's/server .*/server 172.10.0.0 255.255.255.0/' $OVPN_DIR/openvpn.conf
 
     echo "[+] Generando PKI..."
     EASYRSA_BATCH=1 ovpn_initpki nopass
 fi
 
-if [ ! -f "$OVPN_DIR/clients/user1.ovpn" ]; then
-    echo "[+] Creando usuario user1..."
+# Clientes y creacion de usuarios
+mkdir -p "$OVPN_DIR/clients"
+
+if [ ! -f "$OVPN_DIR/pki/private/user1.key" ]; then
+    echo "[+] Creando usuario 1..."
     easyrsa build-client-full user1 nopass
-    ovpn_getclient user1 > $OVPN_DIR/clients/user1.ovpn
 fi
+
+if [ ! -f "$OVPN_DIR/pki/private/user2.key" ]; then
+    echo "[+] Creando usuario 2..."
+    easyrsa build-client-full user2 nopass
+fi
+
+# Exportar a local
+echo "[+] Exportando clientes..."
+
+ovpn_getclient user1 > "$OVPN_DIR/clients/user1.ovpn"
+ovpn_getclient user2 > "$OVPN_DIR/clients/user2.ovpn"
 
 echo "[+] Arrancando servidor OpenVPN..."
 exec ovpn_run
