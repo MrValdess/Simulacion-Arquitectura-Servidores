de05ffd9f8804dcbccaa952ef7cccf6d84edb438
Pablo Garcia <pablocns2002@gmail.com>
2026-06-02 12:34:56 +0200

    Apache done
    No se ha probado con vpn debido al firewall no funcionando

--- apache/apacheinit.sh
+++ apache/apacheinit.sh
@@ -3,12 +3,10 @@ set -e
 
 echo "Instalando dependencias de Apache..."
 
-apt update && apt install -y smbclient ftp
+apt update && apt install -y smbclient ftp dnsutils iputils-ping
 
 a2enmod rewrite
 a2enmod auth_basic
-
-a2dissite 000-default.conf
 
 a2ensite www.conf
 a2ensite intranet.conf
@@ -16,7 +14,6 @@ a2ensite intranet.conf
 htpasswd -bc /etc/apache2/.htpasswd documentos 'GraTiS!'
 
 apache2ctl configtest
-apache2ctl restart
 
 echo "Iniciando Apache..."
 
